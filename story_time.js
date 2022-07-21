72265f4168371d82998082d6efebdf79594fa5d7
MITfanboy <109764861+MITfanboy@users.noreply.github.com>
2022-07-21 23:56:17 +0200

    Add files via upload

---
+++ story_time.js
@@ -0,0 +1,28 @@
+// TODO: Create 4-String variables to introduce develop your story.
+var attacker = 'Jane';
+var city = 'Leipzig';
+var country = 'Germany';
+var my_age = 'twenty';
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+var time_1 = '8:00';
+var time_2 = '8:10';
+var time_3 = '8:23';
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+var items = ["sword", "rock", "pocket knife"];
+
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+var arrested = false;
+
+console.log('boolean arrested: '+arrested);
+console.log('This story happened in '+city+', '+country+', when I was '+my_age+' years old.');
+console.log(attacker+' attacked me with a stick, so I defended myself with a '+items[1]+'.');
+console.log('Then, '+attacker+' decided to attack me with a bat, so I defended myself with a '+items[2]+'.');
+console.log('Then, '+attacker+' decided to attack me with a kitchen knife, so I defended myself with a '+items[0]+'.');
+console.log('Then, I knocked '+attacker+' out at '+time_1+'.');
+console.log('I called the police at '+time_2+'.');
+console.log('The police arrived and arrested '+attacker+' at '+time_3+'.');
+arrested = true;
+console.log('boolean arrested: '+arrested);
